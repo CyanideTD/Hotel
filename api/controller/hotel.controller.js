2be87b9bae1a6de758c168a77a2c2022f77ca69a
CyanideTD <qchen10@stevens.edu>
2017-08-04 15:44:02 -0400

    request query

--- api/controller/hotel.controller.js
+++ api/controller/hotel.controller.js
@@ -1,17 +1,32 @@
 var hotelsData = require('../data/hotel-data.json');
 
 module.exports.getHotels = function (req, res) {
-	console.log("Get all hotels");
-	res
-		.status(200)
-		.json( hotelsData );
+  console.log("Get all hotels");
+  console.log(req.query);
+
+  var offsets = 0;
+  var counts = 5;
+
+  if (req.query && req.query.offset) {
+  	offsets = parseInt(req.query.offset, 10);
+  }
+
+  if (req.query && req.query.count) {
+  	counts = parseInt(req.query.count, 10);
+  }
+
+  var returnData = hotelsData.slice(offsets, offsets + counts);
+
+  res
+	.status(200)
+	.json( returnData );
 };
 
 module.exports.hotelsGetOne = function (req, res) {
-	var hotelId = req.params.hotelId;
-	console.log("Get Hotel " + hotelId);
-	var hotel = hotelsData[hotelId];
-	res
-		.status(200)
-		.json( hotel );
+  var hotelId = req.params.hotelId;
+  console.log("Get Hotel " + hotelId);
+  var hotel = hotelsData[hotelId];
+  res
+	.status(200)
+	.json( hotel );
 };
--- api/routes/index.js
+++ api/routes/index.js
@@ -4,12 +4,11 @@ var router = express.Router();
 var ctrlHotels = require('../controller/hotel.controller.js');
 
 router
-	.route('/hotels')
-	.get(ctrlHotels.getHotels);
+  .route('/hotels')
+  .get(ctrlHotels.getHotels);
 
 router
-	.route('/hotels/:hotelId')
-	.get(ctrlHotels.hotelsGetOne);
-
+  .route('/hotels/:hotelId')
+  .get(ctrlHotels.hotelsGetOne);
 
 module.exports = router;
