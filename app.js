7d5861b86814c73ddfb5742e906ba1e20a94d431
CyanideTD <qchen10@stevens.edu>
2017-08-04 00:15:16 -0400

    homepage

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-console.log("It works");
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.set('port', 3000);
+
+app.get('/', function(req, res) {
+    console.log('Get the homepage');
+    res
+    	.status(200)
+    	.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/json', function(req, res) {
+    console.log('Get the homepage');
+    res
+    	.status(200)
+    	.json({"json": true});
+});
+
+app.get('/file', function(req, res) {
+    console.log('Get the homepage');
+    res
+    	.status(200)
+    	.sendFile(path.join(__dirname, 'app.js'));
+});
+
+var server = app.listen(app.get('port'), function() {
+	var port = server.address().port;
+	console.log('Magic happens on port ' + port);
+});
+console.log('Me first');
---
+++ readme.md
@@ -0,0 +1,1 @@
+This is my first MEAN-STACK web app
